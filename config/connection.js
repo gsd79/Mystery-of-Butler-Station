3252cabd598cfd5258ea8266ea241e6d44876254
White Tiger <dayley.zimmermann@gmail.com>
2022-03-08 10:34:30 -0500

    changed PW to PASSWORD in connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
