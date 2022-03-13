6f174c251be3421fc96a49ad1e18d3cab8cc5eed
Grace <cgracesharpe@gmail.com>
2022-03-12 21:38:37 -0500

    GD troubleshooting

--- models/User.js
+++ models/User.js
@@ -1,24 +1,29 @@
 const { Model, DataTypes } = require('sequelize');
-
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
+// TODO: SHOULD WE CREATE CHAR MODEL AND REF IN USER TABLE?
+// HAVE A SEP SCREEN FOR CHAR SELECTION
+// create fields/columns for User model
 User.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
-    firstName: {
+    name: {
       type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
@@ -28,17 +33,40 @@ User.init(
         isEmail: true
       }
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     age: {
       type: DataTypes.INTEGER,
       allowNull: false
-    }
+    },
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: 'user'
   }
 );
 
