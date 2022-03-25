59e61b40b3d0cb30a3985b1860acfe165e275fee
Grace <cgracesharpe@gmail.com>
2022-03-25 12:52:21 -0400

    GD: got questions to appear on new layout

--- controllers/html/home-routes.js
+++ controllers/html/home-routes.js
@@ -21,24 +21,5 @@ router.get('*', (req, res) => {
   res.render('login', {});
 });
 
-// router.get('/mystery-of-butler-station', (req, res) => {                                                                            
-//   res.sendFile(path.join(__dirname, '../../public/mystery-of-butler-station.html'));
-// });
-
-// router.get('/login', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../views/homepage'));
-// });
-
-// router.get('/signup', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../views/signup'));
-// });
-
-// router.get('/character-creation', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../public/character-creation.html'));
-// });
-
-// router.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../views/login'));
-// });
 
 module.exports = router;
--- public/javascript/character-selection.js
+++ public/javascript/character-selection.js
@@ -1,4 +1,4 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
     panel.addEventListener('click',() => {
@@ -12,4 +12,31 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+
+
+
+async function newCharSelect(event) {
+    event.preventDefault();
+
+    const character_id = document.querySelector('.panel.active').dataset.value;
+      //console.log(character_id);
+    
+    const response = await fetch('/api/users/character-selection', {
+        method: 'POST',
+        body: JSON.stringify({
+            character_id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if (response.ok) {
+        document.location.replace('/mystery-of-butler-station')
+    } else {
+        console.log('You are NOT signed up')
+    }
+}
+
+document.getElementById("playbtn").addEventListener("click", newCharSelect);
+
 
